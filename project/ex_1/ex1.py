947e04a096e0faf8aa224e51e9d62a4420e5acb0
edouard-brosse <edouard.brosse@epitech.eu>
2024-03-06 13:08:45 +0100

    [EDIT]

--- project/ex_1/ex1.py
+++ project/ex_1/ex1.py
@@ -1,14 +1,9 @@
 import numpy as np
 import pandas as pd
 
-# Set random seed for reproducibility
 np.random.seed(42)
-
-# Generate dataset
 num_rows = 300
 num_columns = 6
-
-# Generate data for each column
 data = {
     'Column1': np.random.normal(loc=0, scale=1, size=num_rows),
     'Column2': np.random.normal(loc=3, scale=2, size=num_rows),
@@ -18,10 +13,8 @@ data = {
     'Column6': np.random.normal(loc=-2, scale=3, size=num_rows)
 }
 
-# Create DataFrame
 df = pd.DataFrame(data)
 
-# Generate correlations
 correlation_data = {
     'Column1': np.random.normal(loc=0, scale=1, size=num_rows),
     'Column2': np.random.normal(loc=0, scale=1, size=num_rows),
@@ -31,9 +24,7 @@ correlation_data = {
     'Column6': np.random.normal(loc=0, scale=1, size=num_rows)
 }
 print("start")
-# Add correlations
 df_corr = df + pd.DataFrame(correlation_data)
 
-# Save to CSV
 df_corr.to_csv('artificial_dataset.csv', index=False)
 print('Dataset saved to CSV')
