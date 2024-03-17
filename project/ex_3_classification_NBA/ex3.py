76daf89ecfa59e4361611f6f4a594de83da88c7f
Esteban Hulin <esteban.hulin@epitech.eu>
2024-03-17 14:32:56 +0100

    [Rm][exo3] rm useless lib

--- project/ex_3_classification_NBA/ex3.py
+++ project/ex_3_classification_NBA/ex3.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
